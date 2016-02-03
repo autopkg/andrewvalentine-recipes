5c3adf01713b1a0a208739e6ba5320c95fe45b05
AV <andrew.r.valentine@gmail.com>
2016-02-03 21:39:08 +0000

    fix identifiers

---
+++ kodi/KodiVersioner.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#
+# Copyright 2013 Greg Neagle
+#
+# Licensed under the Aparche License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os.path
+import subprocess
+import re
+
+from autopkglib import ProcessorError
+from autopkglib.DmgMounter import DmgMounter
+import FoundationPlist
+
+__all__ = ["KodiVersioner.py"]
+
+
+class KodiVersioner(DmgMounter):
+    """Extracts version of Kodi, removing errorneous trailing characters"""
+    description = __doc__
+
+    input_variables = {
+        "input_plist_path": {
+            "required": True,
+            "description":
+                ("File path to Kodi plist, inside the .dmg "
+                 "which will be mounted."),
+        },
+        "plist_version_key": {
+            "required": True,
+            "default": "CFBundleShortVersionString",
+            "description":
+                ("Which plist key to use; defaults to "
+                 "CFBundleShortVersionString"),
+        },
+    }
+    output_variables = {
+        "version": {
+            "description": "Version of the item.",
+        },
+    }
+
+    def main(self):
+        """Return a version for file at input_plist_path"""
+        # Check if we're trying to read something inside a dmg.
+        (dmg_path, dmg, dmg_source_path) = (
+            self.parsePathForDMG(self.env['input_plist_path']))
+        try:
+            if dmg:
+                # Mount dmg and copy path inside.
+                mount_point = self.mount(dmg_path)
+                input_plist_path = os.path.join(mount_point, dmg_source_path)
+            else:
+                # just use the given path
+                input_plist_path = self.env['input_plist_path']
+            if not os.path.exists(input_plist_path):
+                raise ProcessorError(
+                    "File '%s' does not exist or could not be read." %
+                    input_plist_path)
+            try:
+                plist = FoundationPlist.readPlist(input_plist_path)
+                version_key = self.env.get("plist_version_key")
+                if version_key.endswith("."):
+                    version_key = version_key[:-1]
+                else:
+                    version_key = plist.get(version_key,"UNKNOWN_VERSION")
+                #else:
+                #    self.env['version'] = plist.get(version_key, "UNKNOWN_VERSION")
+                self.output("Found version %s in file %s"
+                            % (self.env['version'], input_plist_path))
+            except FoundationPlist.FoundationPlistException, err:
+                raise ProcessorError(err)
+
+        finally:
+            if dmg:
+                self.unmount(dmg_path)
+
+
+if __name__ == '__main__':
+    PROCESSOR = Versioner()
+    PROCESSOR.execute_shell()
